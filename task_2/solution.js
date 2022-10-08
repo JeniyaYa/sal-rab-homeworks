b1d0d9f1d9d51562f8bfd77409b299303544dfd7
Evgeniya Yazvicheva <eugenya_my@mail.ru>
2022-10-08 14:41:49 +0300

    Task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,6 +5,20 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
+    let shippingSum;
+    if(productsSum == 0) {
+        shippingSum = 0;
+    } 
+    
+    if(productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }
+
+    if(productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
+
+    
     // создайте переменную shippingSum
 
     // если productsSum равно 0,
@@ -28,6 +42,14 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
+    let discountSum;
+    if(productsSum >= discountMinSum) {
+        discountSum = productsSum / 100 * discountPart;
+    }else{
+        discountSum = 0;
+    }
+
+
     // создайте переменную discountSum
 
     // если productsSum больше или равно discountMinSum,
@@ -45,12 +67,27 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = productsSum - discountSum;
+
     // создайте переменную totalSum
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+
+    totalSum = totalSum + shippingSum;
+     
+    let freeShipping;
+    freeShipping = !shippingSum;
+    
+    
+
+
+
+
 
     // прибавьте к totalSum значение shippingSum
 
